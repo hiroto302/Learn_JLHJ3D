482dd469bee6f5fbd3b9e4055e3438e060709066
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-07 12:36:47 +0900

    Create GameEnding.cs

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1.0f;
+    public float displayImageDuration = 1.0f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    bool m_IsPlayerExit; //初期値 false
+    float m_Timer;       //初期値 0
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            m_IsPlayerExit = true;
+        }
+    }
+    void Update ()
+    {
+        if (m_IsPlayerExit)
+        {
+            EndLevel();
+        }
+    }
+
+    void EndLevel ()
+    {
+        m_Timer += Time.deltaTime;
+        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            Application.Quit();
+        }
+    }
+
+}
+
+
